afc7b96cc8888f85290aa07b9baf3ee26db44e6a
vzvu3k6k <vzvu3k6k@gmail.com>
2015-01-22 04:41:52 +0900

    Change the way to calc offset

--- main.rb
+++ main.rb
@@ -10,19 +10,17 @@ unless ast.type == :begin
 end
 
 new_code = code.dup
-offset = 0
 sorted = ast.children.sort_by{|child|
   code[child.loc.expression.begin_pos .. child.loc.expression.end_pos]
 }
 ast.children.zip(sorted).each{|old, new|
+  offset = new_code.size - code.size
   old_begin = old.loc.expression.begin_pos + offset
   old_end   = old.loc.expression.end_pos   + offset
   new_begin = new.loc.expression.begin_pos
   new_end   = new.loc.expression.end_pos
 
   new_code[old_begin .. old_end] = code[new_begin .. new_end]
-
-  offset += (new_end - new_begin) - (old_end - old_begin)
 }
 
 print new_code
