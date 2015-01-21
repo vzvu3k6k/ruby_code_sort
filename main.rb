9a6992ea402f160805ff1ffef8e04a1fb863e87f
vzvu3k6k <vzvu3k6k@gmail.com>
2015-01-22 04:37:29 +0900

    init

---
+++ main.rb
@@ -0,0 +1,27 @@
+require 'parser/current'
+require 'unparser'
+
+code = $stdin.read
+ast = Parser::CurrentRuby.parse(code)
+
+# If there is only one statement, returns code as it is.
+unless ast.type == :begin
+  print code
+  exit
+end
+
+new_code = code.dup
+offset = 0
+sorted = ast.children.sort_by{|i| Unparser.unparse(i)}
+ast.children.zip(sorted).each{|old, new|
+  old_begin = old.loc.expression.begin_pos + offset
+  old_end   = old.loc.expression.end_pos   + offset
+  new_begin = new.loc.expression.begin_pos
+  new_end   = new.loc.expression.end_pos
+
+  new_code[old_begin .. old_end] = code[new_begin .. new_end]
+
+  offset += (new_end - new_begin) - (old_end - old_begin)
+}
+
+print new_code
