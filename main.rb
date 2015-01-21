8accb92e659b40a29a6c0ce3811158a9c7fcbc16
vzvu3k6k <vzvu3k6k@gmail.com>
2015-01-22 05:37:28 +0900

    Extract sort code into method

--- main.rb
+++ main.rb
@@ -1,26 +1,28 @@
 require 'parser/current'
 
-code = $stdin.read
-ast = Parser::CurrentRuby.parse(code)
+def sort(code)
+  ast = Parser::CurrentRuby.parse(code)
 
-# If there is only one statement, returns code as it is.
-unless ast.type == :begin
-  print code
-  exit
+  # If there is only one statement, returns code as it is.
+  return code unless ast.type == :begin
+
+  new_code = code.dup
+  sorted = ast.children.sort_by{|child|
+    code[child.loc.expression.begin_pos .. child.loc.expression.end_pos]
+  }
+  ast.children.zip(sorted).each{|old, new|
+    offset = new_code.size - code.size
+    old_begin = old.loc.expression.begin_pos + offset
+    old_end   = old.loc.expression.end_pos   + offset
+    new_begin = new.loc.expression.begin_pos
+    new_end   = new.loc.expression.end_pos
+
+    new_code[old_begin .. old_end] = code[new_begin .. new_end]
+  }
+
+  new_code
 end
 
-new_code = code.dup
-sorted = ast.children.sort_by{|child|
-  code[child.loc.expression.begin_pos .. child.loc.expression.end_pos]
-}
-ast.children.zip(sorted).each{|old, new|
-  offset = new_code.size - code.size
-  old_begin = old.loc.expression.begin_pos + offset
-  old_end   = old.loc.expression.end_pos   + offset
-  new_begin = new.loc.expression.begin_pos
-  new_end   = new.loc.expression.end_pos
-
-  new_code[old_begin .. old_end] = code[new_begin .. new_end]
-}
-
-print new_code
+if $0 == __FILE__
+  print sort($stdin.read)
+end
